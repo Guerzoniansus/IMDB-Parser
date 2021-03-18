4761bb68b3d6d2796cc690861f1f361ce411fdc8
erikvanderhorst <erikvanderhorst@hotmail.com>
2021-03-18 12:21:05 +0100

    filter actors from data and create actors.csv

--- src/parsers/ActorsParser.java
+++ src/parsers/ActorsParser.java
@@ -1,27 +1,31 @@
 package parsers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ActorsParser implements ParserStrategy {
     @Override
     public List<String> parse(List<String> data) {
+        List<String> filteredData = new ArrayList<>();
 
         //if data[i] does not contain actor or actress remove line from data.
+        for (String s : data) {
+            if(s.contains("actor") || s.contains("actress"))
+                filteredData.add(s);
+        }
 
-
-        data.replaceAll(line -> {
+        filteredData.replaceAll(line -> {
             String[] items = line.split("\t"); // Splits per tab
 
             for (int i = 0; i < items.length; i++) {
                 if (items[i].contains(",")) {
                     items[i] = "\"" + items[i] + "\"";
                 }
-
             }
 
             return String.join(",", items);
         });
 
-        return data;
+        return filteredData;
     }
 }

0efcae207250f46669ae7fc2189f8c62ff278060
erikvanderhorst <erikvanderhorst@hotmail.com>
2021-03-19 12:51:22 +0100

    merge countries with title_basics

--- src/parsers/TitleParser.java
+++ src/parsers/TitleParser.java
@@ -3,8 +3,7 @@ package parsers;
 import file_utils.FileLoader;
 import file_utils.LoaderListStrategy;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,24 +12,34 @@ public class TitleParser implements ParserStrategy {
 
     @Override
     public List<String> parse(List<String> data) {
-        List<String> CountryFile = FileLoader.getInstance().loadFile("countries.list", new LoaderListStrategy());
-        List<String> MoviesAndCountries = getMovieAndCountry(CountryFile);
-
+        List<String> countryFile = FileLoader.getInstance().loadFile("countries.list", new LoaderListStrategy());
+        List<String> moviesAndCountries = getMovieAndCountry(countryFile);
 
         data.set(0, data.get(0).replace("tconst", "titleID")); // replace tconst with titleID
+
 
         List<String> filteredData;
         filteredData = removeGenre(removeSeries(data)); // remove genre and series from data
         filteredData.set(0, filteredData.get(0) + "country");
+        HashMap<String, String> titles = new HashMap<>();
+
+        for (String line : filteredData) {
+            String name = line.split("\t")[3];
+            titles.put(name, line);
+        }
+
+
+        filteredData = addCountriesToList(titles, moviesAndCountries);
 
 
         filteredData.replaceAll(line -> {
             String[] items = line.split("\t"); // Splits per tab
 
-//            for (String movie : MoviesAndCountries) {
-//                if (movie.split("\t")[0].equals(items[2]))
-//                    items[items.length-1] += movie.split("\t")[1];
-//            }
+            for (int i = 0; i < items.length; i++) {
+                if (items[i].contains(",")) {
+                    items[i] = "\"" + items[i] + "\"";
+                }
+            }
 
             return String.join(",", items);
         });
@@ -67,6 +76,41 @@ public class TitleParser implements ParserStrategy {
 
             result.add(movieName + "\t" + region[region.length - 1]);
         }
+        return result;
+    }
+
+    private List<String> addCountriesToList(HashMap<String, String> titles, List<String> countries) {
+        HashSet<String> checkedCountryTitles = new HashSet<>();
+        int lineCount = 0;
+        for (String line : countries) {
+            if (lineCount > 14) { // data starts at line 15
+                String name = line.split("\t")[0];
+                String country = line.split("\t")[1];
+                if (titles.containsKey(name)) {
+                    if (checkedCountryTitles.contains(name))
+                        continue;
+                    else {
+                        checkedCountryTitles.add(name);
+                        String oldData = titles.get(name);
+                        String newData = oldData + country;
+                        titles.put(name, newData);
+                    }
+                }
+            }
+            if (lineCount <= 14)
+                lineCount++;
+        }
+
+        titles.forEach((index, value) -> {
+            if (!checkedCountryTitles.contains(index)) {
+                checkedCountryTitles.add(index);
+                String newData = value + "\\N";
+                titles.put(index, newData);
+            }
+        });
+
+        ArrayList<String> result = new ArrayList<>(titles.values());
+        Collections.sort(result);
         return result;
     }
 }

d8626900ba7fa5c22f9bc189bbef570d34f72410
qlimax89 <Kevin_25jan@hotmail.com>
2021-03-20 18:22:19 +0100

    actorparser begin (with heap error)

--- src/ParserProgram.java
+++ src/ParserProgram.java
@@ -1,4 +1,5 @@
 import file_utils.*;
+import parsers.ActorParser;
 import parsers.Parser;
 import parsers.RatingsParser;
 import parsers.TestParser;
@@ -21,8 +22,9 @@ public class ParserProgram {
 
         parsers = new ArrayList<>();
 
-        parsers.add(new Parser("test.tsv", new TestParser()));
-        parsers.add(new Parser("ratings.tsv", new RatingsParser()));
+        //parsers.add(new Parser("test.tsv", new TestParser()));
+        //parsers.add(new Parser("ratings.tsv", new RatingsParser()));
+        parsers.add(new Parser("title_principals.tsv", new ActorParser()));
 
         // parsers.add(new Parser("ratings.tsv", new RatingsParser()));
         // parsers.add(new Parser("movies.tsv", new MoviesParser()));

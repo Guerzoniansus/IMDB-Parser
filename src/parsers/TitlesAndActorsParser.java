fcd6b0fc329984a2c6d062a9b7608c70bc443224
qlimax89 <Kevin_25jan@hotmail.com>
2021-03-21 19:39:42 +0100

    added comments to TitlesAndActorParser

--- src/parsers/TitlesAndActorsParser.java
+++ src/parsers/TitlesAndActorsParser.java
@@ -17,31 +17,32 @@ public class TitlesAndActorsParser implements ParserStrategy {
 
     @Override
     public String parse(String line) {
-
+        //checks every line
         if (!checkFirstLine) {
             checkFirstLine = true;
             return replaceColumnNames();
         }
-
+        //checks if person is a actor
         if ((line.contains("actor") || line.contains("actress")) == false) {
             return null;
         }
-
+        //lines split
         String[] items = line.split("\t");
+
         String PersonID = items[PERSON_ID_COL_INDEX];
 
         String[] FilmIDs = items[TITLE_ID_COL_INDEX].split(",");
 
         String returnString = "";
 
-
+        //output sets on every line personID + titleID
         for (String TitleID : FilmIDs) {
             returnString += PersonID + "," + TitleID + "\n";
         }
 
         return returnString;
     }
-
+    
     private String replaceColumnNames() {
         return "actorID" + "," + "titleID";
     }
